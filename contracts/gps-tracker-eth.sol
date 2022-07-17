4ac4233671b534c1b6eff32cfeed09412088acd4
abeselomg <abeselomgebrekidan12@gmail.com>
2022-07-18 01:40:28 +0300

    connected with react app

--- contracts/gps-tracker-eth.sol
+++ contracts/gps-tracker-eth.sol
@@ -1,11 +1,14 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.15;
 
+//e9445f523f0940059e14a148a598ef3d : PROJECT ID
+//c1b5a2f4325c473a93d38033851fb4be : PROJECT SECRET
+
 contract gpsContract {
     address public owner;
     address[] private Employees;
 
-    constructor() {
+    constructor() payable{
         owner = msg.sender;
     }
 
@@ -17,15 +20,18 @@ contract gpsContract {
     struct DateTime {
         uint8 hour;
         uint8 minute;
-        uint8 second;
+        
     }
     struct EmployeeContract {
         //address emp_add;
-        GpsCordinates emp_cord;
+        uint256 latitude;
+        uint256 longtude;
         bool isactive;
-        DateTime startTime;
-        DateTime endTime;
-        uint8 amount;
+        uint8 start_hour;
+        uint8 start_minute;
+        uint8 end_hour;
+        uint8 end_minute;
+        uint amount;
         uint8 validRadius;
     }
 
@@ -38,38 +44,25 @@ contract gpsContract {
 
     //***************************UTILITY FUNCTIONS******************************* */
 
-    function checkEmployeeExists(address emp) private view returns (bool) {
+    function checkEmployeeExists(address emp) public view returns (bool) {
         for (uint256 i = 0; i < Employees.length; i++) {
             if (Employees[i] == emp) return true;
         }
         return false;
     }
 
-    function time_is_valid(
+    function time_has_passed(
         DateTime memory fir,
-        DateTime memory start_time,
         DateTime memory end_time
     ) private pure returns (bool) {
-        if (fir.hour > end_time.hour || fir.hour < start_time.hour) {
-            return false;
-        } else if (fir.hour == end_time.hour || fir.hour == start_time.hour) {
-            if (
-                fir.minute > end_time.minute || fir.minute < start_time.minute
-            ) {
-                return false;
-            } else if (
-                fir.minute == end_time.minute || fir.minute == start_time.minute
-            ) {
-                return true;
-            } else {
-                return true;
-            }
-        } else {
+        if (fir.hour > end_time.hour ) {
             return true;
-        }
+        }  
 
         return false;
     }
+
+
 
     function sqrt(uint256 x) private pure returns (uint256 y) {
         uint256 z = (x + 1) / 2;
@@ -99,57 +92,34 @@ contract gpsContract {
 
     //***************************EMPLOYER FUNCTIONS******************************* */
 
-    function addEmployee(address emp_address) private {
+    function addEmployee(address emp_address) public {
         if (!checkEmployeeExists(emp_address)) {
             Employees.push(emp_address);
         }
-        // if(!checkEmployeeExists(emp_address)){
-        //     Employees.push(emp_address);
-        //     EmployeeContrInfo[emp_address].emp_cord.latitude=0;
-        //     EmployeeContrInfo[emp_address].emp_cord.longtude=0;
-        //     EmployeeContrInfo[emp_address].isactive=false;
-        //     EmployeeContrInfo[emp_address].startTime.hour=0;
-        //     EmployeeContrInfo[emp_address].startTime.minute=0;
-        //     EmployeeContrInfo[emp_address].startTime.second=0;
-        //     EmployeeContrInfo[emp_address].endTime.hour=0;
-        //     EmployeeContrInfo[emp_address].endTime.minute=0;
-        //     EmployeeContrInfo[emp_address].endTime.second=0;
-        //     EmployeeContrInfo[emp_address].amount=0;
-        //     EmployeeContrInfo[emp_address].validRadius=0;
-        // }
     }
+
+    
 
     function createContract(
         address emp_add,
         uint256 longtude_val,
         uint256 latitude_val,
-        uint8 amount,
+        uint amount,
         uint8 start_hour,
         uint8 start_minute,
-        uint8 start_second,
+        
         uint8 end_hour,
         uint8 end_minute,
-        uint8 end_second,
+        
         uint8 radius
     ) public payable {
-        //Employees.push(emp_add); 
-        //add to valid emps
-
-        GpsCordinates memory gps_cor = GpsCordinates(
-            latitude_val,
-            longtude_val
-        );
-        DateTime memory start_time = DateTime(
-            start_hour,
-            start_minute,
-            start_second
-        );
-        DateTime memory end_time = DateTime(end_hour, end_minute, end_second);
-
-        EmployeeContrInfo[emp_add].emp_cord = gps_cor;
+        EmployeeContrInfo[emp_add].latitude = latitude_val;
+        EmployeeContrInfo[emp_add].longtude = longtude_val;
         EmployeeContrInfo[emp_add].isactive = true;
-        EmployeeContrInfo[emp_add].startTime = start_time;
-        EmployeeContrInfo[emp_add].endTime = end_time;
+        EmployeeContrInfo[emp_add].start_hour=start_hour;
+        EmployeeContrInfo[emp_add].start_minute = start_minute;
+        EmployeeContrInfo[emp_add].end_hour = end_hour;
+        EmployeeContrInfo[emp_add].end_minute = end_minute;
         EmployeeContrInfo[emp_add].amount = amount;
         EmployeeContrInfo[emp_add].validRadius = radius;
     }
@@ -160,49 +130,37 @@ contract gpsContract {
         EmployeeContrInfo[emp_add].isactive = false;
     }
 
-    function pay(address payable emp_add) public {
+    function pay(address  emp_add) public payable{
         EmployeeContrInfo[emp_add].isactive = false;
-        emp_add.transfer(EmployeeContrInfo[emp_add].amount);
+       payable(emp_add).transfer(EmployeeContrInfo[emp_add].amount);
+        
+
+    
     }
+
+
+
+    function remove_contract(address emp_add) public{
+        delete EmployeeContrInfo[emp_add];
+    }
+
 
     // For Employee
 
     function sendCoordinates(
         uint256 lat,
-        uint256 long,
-        uint8 hour,
-        uint8 minute,
-        uint8 second
+        uint256 long
     ) public {
         require(!checkEmployeeExists(msg.sender),"You are not an employee");
 
         GpsCordinates memory gps_cor = GpsCordinates(lat, long);
 
-        GpsCordinates memory orig_cor = EmployeeContrInfo[msg.sender].emp_cord;
+        GpsCordinates memory orig_cor=GpsCordinates(EmployeeContrInfo[msg.sender].latitude,EmployeeContrInfo[msg.sender].longtude);
         uint256 distance = calculateDistance(gps_cor, orig_cor);
 
-        if (distance < EmployeeContrInfo[msg.sender].validRadius) {
-
-//Pay button
-
-
-
-            //check if time is valid
-            DateTime memory cur_time = DateTime(hour, minute, second);
-            DateTime memory start_time = EmployeeContrInfo[msg.sender]
-                .startTime;
-            DateTime memory end_time = EmployeeContrInfo[msg.sender].endTime;
-            bool isValidTime = time_is_valid(cur_time, start_time, end_time);
-            if (isValidTime) {
-                //Logic to pay the employee and terminate the contract if the time past the end time
-            } else {
-                terminateContract(msg.sender);
-            }
-
-
-        } else {
+        if (distance > EmployeeContrInfo[msg.sender].validRadius) {
             terminateContract(msg.sender);
-        }
+        } 
         // if inside area?check for time and activeness
     }
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,11 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+  solidity: "0.8.15",
+  networks: {
+    rinkeby: {
+      url: "https://rinkeby.infura.io/v3/e9445f523f0940059e14a148a598ef3d", //Infura url with projectId
+      accounts: ["7c8a72c1844ea09d44a54695cd6bcac327aa80651c3bed34488f0d672e6b10fe"], // add the account that will deploy the contract (private key)
+    },
+  },
 };
